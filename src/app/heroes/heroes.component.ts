1752c18500e25af358524a400dd7d6ee6e0686fe
AnnKemp <ann.kemp@scarlet.be>
2020-06-26 13:46:57 +0200

    making list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
-
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({ // decorator
   selector: 'app-heroes',
@@ -9,10 +9,12 @@ import { Hero } from '../hero'
 })
 export class HeroesComponent implements OnInit {
  // hero='Windstorm'; //property  wordt gerefactored naar model van de interface
-  hero: Hero= { // en hero wordt getypeerd gevormd naar het model van de interface
+  /* hero: Hero= { // en hero wordt getypeerd gevormd naar het model van de interface
     id: 1,
     name: 'Windstorm'
-  }
+  } */
+ heroes = HEROES; // nu wordt ipv één zoals hierboven een hele lijst geïmporteerd van mock-heroes - een array met objecten in
+
 
   constructor() { }
 
